01ce04394a7fcee741fd4d034bbb2058cb901006
Bhavika Patel <bhavij28@gmail.com>
2021-06-07 15:14:58 -0400

    udpated server.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,77 +1,56 @@
-const FILES_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/index.js',
-    '/styles.css',
-    '/db.js',
-    '/manifest.json',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png',
-  ];
-  
-  const PRECACHE = 'precache-v1';
-  const RUNTIME = 'runtime';
-  
-  self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches
-        .open(PRECACHE)
-        .then((cache) => {
-          console.log("precache successful!");
-          return cache.addAll(FILES_TO_CACHE)})
-        .then(self.skipWaiting())
-    );
-  });
-  
-  // The activate handler takes care of cleaning up old caches.
-  self.addEventListener('activate', (event) => {
+var CACHE_NAME = "my-site-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
 
-    event.waitUntil(
-      caches.keys().then(keyList => {
-        return Promise.all(
-          keyList.map(key => {
-            if (key !== PRECACHE && key !== RUNTIME) {
-              console.log("Removing old cache", key);
-              return caches.delete(key);
+var urlsToCache = [
+  "/",
+  "/db.js",
+  "/index.js",
+  "/manifest.json",
+  "/styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png"
+];
+
+self.addEventListener("install", function(event) {
+  
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+            if (response.status === 200) {
+              cache.put(event.request.url, response.clone());
             }
+
+            return response;
           })
-        );
-      })
+          .catch(err => {
+            return cache.match(event.request);
+          });
+      }).catch(err => console.log(err))
     );
-  
-    self.clients.claim();
-  });
-  
-  // fetch
-  self.addEventListener("fetch", evt => {
-      if(evt.request.url.includes('/api/' && evt.request.method === "GET")) {
-          console.log("Service Worker fetching data....", evt.request.url);
-   
-  //if request made to API, clone response and store in cache
-  evt.respondWith(
-                  caches.open(RUNTIME).then(cache => {
-                  return fetch(evt.request)
-                  .then(response => {
-                      if (response.status === 200){
-                          cache.put(evt.request, response.clone());
-                      }
-                      return response;
-                  })
-                  .catch(err => {
-                    console.log(err);
-                      return cache.match(evt.request);
-                  });
-              })
-              );
-              return;
-          }
-  
-          //if request is not for the API, send cache data
-  evt.respondWith(
-      caches.open(PRECACHE).then( cache => {
-        return cache.match(evt.request).then(response => {
-          return response || fetch(evt.request);
-        });
-      })
-    );
-  });
+
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          return caches.match("/");
+        }
+      });
+    })
+  );
+});
